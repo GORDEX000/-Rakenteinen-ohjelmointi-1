eff01a1b3284a9210b46b5544080604794a7aa76
GORDEX000 <ezatullahnuurzaii@outlook.com>
2024-11-28 21:33:52 +0200

    done 4 tasks

---
+++ Tasks/2/2.py
@@ -0,0 +1,18 @@
+print("give me an number")
+input1 = input()
+print("give me an second number")
+input2 = input()
+
+
+sum = int(input1) + int(input2)
+minus = int(input1) - int(input2)
+multiply = int(input1) * int(input2)
+division = int(input1) / int(input2)
+remainder = int(input1) % int(input2)
+
+
+print("Sum: ", sum)
+print("Minus: ", minus)
+print("multiply: ", multiply)
+print("division: ", int(division))
+print("remainder: ", int(remainder))
---
+++ Tasks/4/4.py
@@ -0,0 +1,8 @@
+print("give me an number")
+input1 = input()
+print("give me an second number")
+input2 = input()
+
+minimum = min(input1, input2)
+
+print("The minimum Number is: ", minimum)
